552212856a4fa8803bad7e72ddc1e81880468253
devepkm <devepkm@gmail.com>
2020-09-08 11:29:13 +0800

    imp KeyboardUtils

--- src/com/devepkm/utils/KeyboardUtils.java
+++ src/com/devepkm/utils/KeyboardUtils.java
@@ -1,4 +1,57 @@
 package com.devepkm.utils;
 
+
+import java.util.Scanner;
+
 public class KeyboardUtils {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static int readOption() {
+
+
+        int parseInt = 0;
+        System.out.print("Option: ");
+        while (sc.hasNext()) {
+            String s = sc.next();
+            try {
+                parseInt = Integer.parseInt(s);
+                if (parseInt <= 4 && parseInt > 0) {
+                    break;
+                }
+            } catch (NumberFormatException e) {
+
+            }
+            System.out.println("Invalid Input!");
+            System.out.print("Option: ");
+        }
+
+
+
+        return parseInt;
+
+
+    }
+
+
+    public static int selectUser() {
+
+
+        int i = 0;
+        while (true) {
+            i = readOption();
+            if (i <= 3){
+                break;
+            }
+            System.out.println("Invalid Input!");
+        }
+        return i;
+    }
+
+
+    public static String readString(){
+        String next = sc.next();
+        return next;
+
+    }
 }
